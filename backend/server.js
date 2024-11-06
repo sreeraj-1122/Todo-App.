30f40c20e05e7afcc38f207ff16f9492f739f3d0
sreeraj <85874548+sreeraj-1122@users.noreply.github.com>
2024-11-06 19:40:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,7 @@ const PORT=4000
 
 //middleware
 app.use(express.json())
-app.use(cors({
-    origin: 'http://localhost:5173', 
-    credentials: true, 
-  }));
+app.use(cors());
 app.use(cookieParser());
 
 //db connection
